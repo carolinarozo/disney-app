441891460d7f9ecc78b6d35663e75436576c2ef2
Carolina rozo <carolinarozosaavedra@gmail.com>
2022-06-05 22:15:51 -0500

    creacion de DTO personaje

--- src/main/java/com/alkemy/disney/disney/dto/PersonajeDTO.java
+++ src/main/java/com/alkemy/disney/disney/dto/PersonajeDTO.java
@@ -3,9 +3,17 @@ package com.alkemy.disney.disney.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 
 public class PersonajeDTO {
+    private Long id;
+    private String nombre;
+    private Long edad;
+    private Long peso;
+    private String historia;
+    private List<PeliculasSeriesDTO> peliculasSeries;
 
 }
